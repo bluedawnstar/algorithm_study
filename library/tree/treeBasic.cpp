b90f523f4894e4a78837b0af6e13ab92f5b6ba7f
youngman <youngman.ro@gmail.com>
2017-03-26 15:04:10 +0900

    converting tree related functions to a struct type

--- library/tree/treeBasic.cpp
+++ library/tree/treeBasic.cpp
@@ -6,145 +6,7 @@
 
 using namespace std;
 
-namespace TreeBasic {
-
-//--------- Common ------------------------------------------------------------
-
-#define MAXN    10000           // TODO: modify the maximum number of nodes
-#define LOGN    15              // TODO: modify LCA table size (log2(MAXN))
-
-int gN;                         // TODO: set a value
-
-vector<int> gE[MAXN];           // TODO: make a tree
-int gP[LOGN][MAXN];             // TODO: set all gP[0][n] to their parent
-                                // parent & acestors
-
-int gLevel[MAXN];               // depth (root is 0)
-
-void clear() {
-    if (gN <= 0)
-        return;
-
-    for (int i = 0; i < gN; i++)
-        gE[i].clear();
-    memset(gLevel, 0, sizeof(gLevel));
-    memset(gP, 0, sizeof(gP));
-}
-
-//--------- DFS ---------------------------------------------------------------
-
-void dfs(int u, int parent) {
-    gP[0][u] = parent;
-
-    for (int v : gE[u]) {
-        if (v == parent)
-            continue;
-        
-        gLevel[v] = gLevel[u] + 1;
-        dfs(v, u);
-    }
-}
-
-void dfsIter(int root) {
-    struct Item {
-        int u;
-        int parent;
-        int vi;         // child index
-    };
-    vector<Item> st;
-    st.reserve(gN);
-
-    st.push_back(Item{ root, -1, -1 });
-    while (!st.empty()) {
-        Item& it = st.back();
-        if (++it.vi == 0) {
-            // enter ...
-            gP[0][it.u] = it.parent;
-        }
-        if (it.vi >= (int)gE[it.u].size()) {
-            // leave ...
-            st.pop_back();
-        } else if (gE[it.u][it.vi] != it.parent) {
-            // recursion
-            int v = gE[it.u][it.vi];
-            gLevel[v] = gLevel[it.u] + 1;
-            st.push_back(Item{ v, it.u, -1 });
-        }
-    }
-}
-
-//-----------------------------------------------------------------------------
-
-void bfs(int root) {
-    vector<bool> visited(gN);
-
-    queue<int> Q;
-    Q.push(root);
-    visited[root] = true;
-    while (!Q.empty()) {
-        int u = Q.front();
-        Q.pop();
-
-        for (int v : gE[u]) {
-            if (visited[v])
-                continue;
-
-            visited[v] = true;
-
-            gP[0][v] = u;
-            gLevel[v] = gLevel[u] + 1;
-            Q.push(v);
-        }
-    }
-}
-
-//--------- LCA ---------------------------------------------------------------
-
-void makeLcaTable() {
-    for (int i = 1; i < LOGN; i++) {
-        for (int j = 0; j < gN; j++) {
-            gP[i][j] = gP[i - 1][gP[i - 1][j]];
-        }
-    }
-}
-
-int climbTree(int node, int dist) {
-    if (dist <= 0)
-        return node;
-    
-    for (int i = 0; dist > 0; i++) {
-        if (dist & 1)
-            node = gP[i][node];
-        dist >>= 1;
-    }
-
-    return node;
-}
-
-int findLCA(int A, int B) {
-    if (gLevel[A] < gLevel[B])
-        swap(A, B);
-
-    A = climbTree(A, gLevel[A] - gLevel[B]);
-
-    if (A == B)
-        return A;
-
-    int bitCnt = 0;
-    for (int x = gLevel[A]; x; x >>= 1)
-        bitCnt++;
-
-    for (int i = bitCnt - 1; i >= 0; i--) {
-        if (gP[i][A] > 0 && gP[i][A] != gP[i][B]) {
-            A = gP[i][A];
-            B = gP[i][B];
-        }
-    }
-
-    return gP[0][A];
-}
-
-}
+#include "treeBasic.h"
 
 /////////// For Testing ///////////////////////////////////////////////////////
 
@@ -154,127 +16,125 @@ int findLCA(int A, int B) {
 #include <iostream>
 #include "../common/iostreamhelper.h"
 
-using namespace TreeBasic;
+#define MAXN    10000           // TODO: modify the maximum number of nodes
+#define LOGN    15              // TODO: modify LCA table size (log2(MAXN))
 
-static void makeTree() {
-    gN = 10;
+static void makeTree(Tree<MAXN, LOGN>& tree) {
+    tree.setVertexCount(10);
     
-    gE[0].push_back(1); gE[1].push_back(0);
-    gE[0].push_back(3); gE[3].push_back(0);
-    gE[1].push_back(4); gE[4].push_back(1);
-    gE[1].push_back(2); gE[2].push_back(1);
-    gE[3].push_back(6); gE[6].push_back(3);
-    gE[3].push_back(7); gE[7].push_back(3);
-    gE[4].push_back(9); gE[9].push_back(4);
-    gE[2].push_back(8); gE[8].push_back(2);
-    gE[2].push_back(5); gE[5].push_back(2);
+    tree.addEdge(0, 1);
+    tree.addEdge(0, 3);
+    tree.addEdge(1, 4);
+    tree.addEdge(1, 2);
+    tree.addEdge(3, 6);
+    tree.addEdge(3, 7);
+    tree.addEdge(4, 9);
+    tree.addEdge(2, 8);
+    tree.addEdge(2, 5);
 }
 
-static void makeLcaTree() {
-    gN = MAXN;
+static void makeLcaTree(Tree<MAXN, LOGN>& tree) {
+    tree.setVertexCount(MAXN);
 
-    gE[0].push_back(1); gE[1].push_back(0);
-    gE[0].push_back(2); gE[2].push_back(0);
+    tree.addEdge(0, 1);
+    tree.addEdge(0, 2);
 
     int i, p = 1;
-    for (i = 3; i < gN / 4; i++) {
-        gE[p].push_back(i); gE[i].push_back(p);
+    for (i = 3; i < tree.mN / 4; i++) {
+        tree.addEdge(p, i);
         p = i;
     }
 
     p = 1;
-    for (; i < gN * 2 / 4; i++) {
-        gE[p].push_back(i); gE[i].push_back(p);
+    for (; i < tree.mN * 2 / 4; i++) {
+        tree.addEdge(p, i);
         p = i;
     }
 
     p = 2;
-    for (; i < gN * 3 / 4; i++) {
-        gE[p].push_back(i); gE[i].push_back(p);
+    for (; i < tree.mN * 3 / 4; i++) {
+        tree.addEdge(p, i);
         p = i;
     }
 
     p = 2;
-    for (; i < gN; i++) {
-        gE[p].push_back(i); gE[i].push_back(p);
+    for (; i < tree.mN; i++) {
+        tree.addEdge(p, i);
         p = i;
     }
 }
 
-static void printData() {
+static void printTree(Tree<MAXN, LOGN>& tree) {
     cout << "level : ";
-    for (int i = 0; i < gN; i++)
-        cout << gLevel[i] << ", ";
+    for (int i = 0; i < tree.mN; i++)
+        cout << tree.mLevel[i] << ", ";
     cout << endl;
 
     cout << "parent : ";
-    for (int i = 0; i < gN; i++)
-        cout << gP[i][0] << ", ";
+    for (int i = 0; i < tree.mN; i++)
+        cout << tree.mP[i][0] << ", ";
     cout << endl;
 }
 
 void testTreeBasic() {
-    return; //TODO: if you want to test a split function, make this line to a comment.
+    //return; //TODO: if you want to test a split function, make this line to a comment.
 
-    //Usage:
-    //    step1 : make a tree (gE, gP[0][x])
+    Tree<MAXN, LOGN> tree;
 
     cout << "-- dfs() vs dfsIter() ----------------------------------" << endl;
-    clear();
-    makeTree(); // make a test tree
-    dfs(0, -1);
-    printData();
+    tree.clear();
+    makeTree(tree); // make a test tree
+    tree.dfs(0, -1);
+    printTree(tree);
 
-    makeTree();
-    dfsIter(0);
-    printData();
+    makeTree(tree);
+    tree.dfsIter(0);
+    printTree(tree);
 
     /*
     cout << "-- dfs() vs dfsIter() - performance test ---------------" << endl;
-    clear();
-    makeTree(); // make a test tree
+    tree.clear();
+    makeTree(tree); // make a test tree
     clock_t start = clock();
     for (int i = 0; i < 1000000; i++) {
-        gCurrTime = 0;
-        dfs(1, 0);
+        tree.dfs(1, 0);
     }
     cout << "elapsed time : " << double(clock() - start) / CLOCKS_PER_SEC << endl;
 
-    clear();
-    makeTree(); // make a test tree
+    tree.clear();
+    makeTree(tree); // make a test tree
     start = clock();
     for (int i = 0; i < 1000000; i++) {
-        gCurrTime = 0;
-        dfsIter(1);
+        tree.dfsIter(1);
     }
     cout << "elapsed time : " << double(clock() - start) / CLOCKS_PER_SEC << endl;
     */
 
     cout << "-- LCA test --------------------------------------------" << endl;
-    clear();        // step1: clear all variables
-    makeLcaTree();  // ... make a test tree
+    tree.clear();       // step1: clear all variables
+    makeLcaTree(tree);  // ... make a test tree
 
-    //dfs(0, -1);
-    dfsIter(0);     // step2: make depth and parent table
-    makeLcaTable(); // step3: make LCA table
+    //tree.dfs(0, -1);
+    tree.dfsIter(0);    // step2: make depth and parent table
+    tree.makeLcaTable();// step3: make LCA table
 
     int errCnt = 0;
     for (int i = 0; i < 100000; i++) {
-        int u = rand() % gN;
-        int v = rand() % gN;
-        int lca = findLCA(u, v);
+        int u = rand() % tree.mN;
+        int v = rand() % tree.mN;
+        int lca = tree.findLCA(u, v);
         int lcaAns;
         if (u == 0 || v == 0) {
             lcaAns = 0;
-        } else if ((u != 2 && u < gN / 2) != (v != 2 && v < gN / 2)) {
+        } else if ((u != 2 && u < tree.mN / 2) != (v != 2 && v < tree.mN / 2)) {
             lcaAns = 0;
-        } else if (u != 2 && u < gN / 2) {
-            if ((u >= gN / 4) != (v >= gN / 4))
+        } else if (u != 2 && u < tree.mN / 2) {
+            if ((u >= tree.mN / 4) != (v >= tree.mN / 4))
                 lcaAns = 1;
             else
                 lcaAns = min(u, v);
         } else {
-            if ((u >= gN * 3 / 4) != (v >= gN * 3 / 4))
+            if ((u >= tree.mN * 3 / 4) != (v >= tree.mN * 3 / 4))
                 lcaAns = 2;
             else
                 lcaAns = min(u, v);
@@ -286,5 +146,4 @@ void testTreeBasic() {
     }
     if (!errCnt)
         cout << "OK!" << endl;
-
 }
---
+++ library/tree/treeBasic.h
@@ -0,0 +1,170 @@
+#pragma once
+
+/*
+<How to use>
+    1) call setCount() to set the number of vertexes
+    2) call add() to construct tree
+    3) call dfs() / dfsIter() or bfs() to set level and parent table
+    4) call makeLcaTable() to create LCA table
+
+    5) call query functions
+*/
+
+// MAXN : modify the maximum number of nodes
+// LOGN : modify LCA table size (log2(MAXN))
+template <int MaxN, int LogN>
+struct Tree {
+    int                 mN;         // the number of vertex
+
+    vector<vector<int>> mE;         // edges (vertex number)
+    vector<vector<int>> mP;         // mP[0][n] points to the parent
+                                    // parent & acestors
+
+    vector<int>         mLevel;     // depth (root is 0)
+
+    //--- tree construction ---------------------------------------------------
+    
+    Tree() : mE(MaxN), mP(LogN, vector<int>(MaxN)), mLevel(MaxN) {
+        mN = 0;
+    }
+
+    void clear() {
+        if (mN <= 0)
+            return;
+
+        for (int i = 0; i < mN; i++)
+            mE[i].clear();
+
+        for (int i = 0; i < LogN; i++)
+            fill(mP[i].begin(), mP[i].end(), 0);
+        fill(mLevel.begin(), mLevel.end(), 0);
+    }
+
+
+    void setVertexCount(int n) {
+        mN = n;
+    }
+
+    void addEdge(int u, int v) {
+        mE[u].push_back(v);
+        mE[v].push_back(u);
+    }
+
+    void addEdgeDirected(int u, int v) {
+        mE[u].push_back(v);
+    }
+
+    //--- DFS -----------------------------------------------------------------
+
+    void dfs(int u, int parent) {
+        mP[0][u] = parent;
+
+        for (int v : mE[u]) {
+            if (v == parent)
+                continue;
+
+            mLevel[v] = mLevel[u] + 1;
+            dfs(v, u);
+        }
+    }
+
+    void dfsIter(int root) {
+        struct Item {
+            int u;
+            int parent;
+            int vi;         // child index
+        };
+        vector<Item> st;
+        st.reserve(mN);
+
+        st.push_back(Item{ root, -1, -1 });
+        while (!st.empty()) {
+            Item& it = st.back();
+            if (++it.vi == 0) {
+                // enter ...
+                mP[0][it.u] = it.parent;
+            }
+            if (it.vi >= (int)mE[it.u].size()) {
+                // leave ...
+                st.pop_back();
+            } else if (mE[it.u][it.vi] != it.parent) {
+                // recursion
+                int v = mE[it.u][it.vi];
+                mLevel[v] = mLevel[it.u] + 1;
+                st.push_back(Item{ v, it.u, -1 });
+            }
+        }
+    }
+
+    //--- BFS -----------------------------------------------------------------
+
+    void bfs(int root) {
+        vector<bool> visited(mN);
+
+        queue<int> Q;
+        Q.push(root);
+        visited[root] = true;
+        while (!Q.empty()) {
+            int u = Q.front();
+            Q.pop();
+
+            for (int v : mE[u]) {
+                if (visited[v])
+                    continue;
+
+                visited[v] = true;
+
+                mP[0][v] = u;
+                mLevel[v] = mLevel[u] + 1;
+                Q.push(v);
+            }
+        }
+    }
+
+    //--- LCA -----------------------------------------------------------------
+
+    void makeLcaTable() {
+        for (int i = 1; i < LogN; i++) {
+            for (int j = 0; j < mN; j++) {
+                int pp = mP[i - 1][j];
+                mP[i][j] = pp < 0 ? pp : mP[i - 1][pp];
+            }
+        }
+    }
+
+    int climbTree(int node, int dist) {
+        if (dist <= 0)
+            return node;
+
+        for (int i = 0; dist > 0; i++) {
+            if (dist & 1)
+                node = mP[i][node];
+            dist >>= 1;
+        }
+
+        return node;
+    }
+
+    int findLCA(int A, int B) {
+        if (mLevel[A] < mLevel[B])
+            swap(A, B);
+
+        A = climbTree(A, mLevel[A] - mLevel[B]);
+
+        if (A == B)
+            return A;
+
+        int bitCnt = 0;
+        for (int x = mLevel[A]; x; x >>= 1)
+            bitCnt++;
+
+        for (int i = bitCnt - 1; i >= 0; i--) {
+            if (mP[i][A] > 0 && mP[i][A] != mP[i][B]) {
+                A = mP[i][A];
+                B = mP[i][B];
+            }
+        }
+
+        return mP[0][A];
+    }
+};
