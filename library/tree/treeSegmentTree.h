da83798d4bf3b53aa3085012109200ca39a13160
youngman <youngman.ro@gmail.com>
2017-06-06 20:12:53 +0900

    making segment tree class simple

--- library/tree/treeSegmentTree.h
+++ library/tree/treeSegmentTree.h
@@ -46,140 +46,95 @@ struct LcmOp {
 // Others('left', 'right', 'nodeLeft', 'nodeRight', 'index') are started from 0
 template <typename T, typename BinOp = function<T(T,T)>>
 struct SegmentTree {
-    int       n;            // the size of array
-    vector<T> rangeValue;   // 
+    int       N;            // the size of array
+    vector<T> tree;         // 
+
     T         defaultValue;
     BinOp     mergeOp;
 
     SegmentTree(int size, T dflt = T())
-        : mergeOp(), defaultValue(dflt) {
-
-        n = size;
-        rangeValue.resize(n * 4);
-        _init(0, n - 1, 1);
+        : N(size), tree(size * 4, dflt), mergeOp(), defaultValue(dflt) {
     }
 
     SegmentTree(int size, BinOp op, T dflt = T())
-        : mergeOp(op), defaultValue(dflt) {
-
-        n = size;
-        rangeValue.resize(n * 4);
-        _init(0, n - 1, 1);
-    }
-
-    SegmentTree(const vector<T>& arr, T dflt = T())
-        : mergeOp(), defaultValue(dflt) {
-
-        n = (int)arr.size();
-        rangeValue.resize(n * 4);
-        _init(arr, 0, n - 1, 1);
-    }
-
-    SegmentTree(const T arr[], int size, T dflt = T())
-        : mergeOp(), defaultValue(dflt) {
-
-        n = size;
-        rangeValue.resize(n * 4);
-        _init(arr, 0, n - 1, 1);
-    }
-
-    SegmentTree(const vector<T>& arr, BinOp op, T dflt = T())
-        : mergeOp(op), defaultValue(dflt) {
-
-        n = (int)arr.size();
-        rangeValue.resize(n * 4);
-        _init(arr, 0, n - 1, 1);
-    }
-
-    SegmentTree(const T arr[], int size, BinOp op, T dflt = T())
-        : mergeOp(op), defaultValue(dflt) {
-
-        n = size;
-        rangeValue.resize(n * 4);
-        _init(arr, 0, n - 1, 1);
-    }
-
-    SegmentTree(SegmentTree&& rhs)
-        : n(rhs.n), rangeValue(std::move(rhs.rangeValue)),
-          defaultValue(rhs.defaultValue), mergeOp(std::move(rhs.mergeOp)) {
+        : N(size), tree(size * 4, dflt), mergeOp(op), defaultValue(dflt) {
     }
 
     // inclusive
-    T _init(int left, int right, int node) {
-        if (left > right)
-            return defaultValue;
-
-        if (left == right)
-            return rangeValue[node] = defaultValue;
-
-        int mid = left + (right - left) / 2;
-        T leftSum = _init(left, mid, node * 2);
-        T rightSum = _init(mid + 1, right, node * 2 + 1);
-
-        return rangeValue[node] = mergeOp(leftSum, rightSum);
+    T build(const vector<T>& v, int left, int right) {
+        return buildSub(v, left, right, 1);
+    }
+    // inclusive
+    T build(const T arr[], int left, int right) {
+        return buildSub(arr, left, right, 1);
     }
 
+    // inclusive
+    T query(int left, int right) {
+        return querySub(left, right, 1, 0, N - 1);
+    }
+
+    // inclusive
+    T update(int index, T newValue) {
+        return updateSub(index, newValue, 1, 0, N - 1);
+    }
+
+    // inclusive
+    T updateRange(int left, int right, T newValue) {
+        return updateRangeSub(left, right, newValue, 1, 0, N - 1);
+    }
+
+private:
     // inclusive
     template <typename U>
-    T _init(const U& arr, int left, int right, int node) {
+    T buildSub(const U& arr, int left, int right, int node) {
         if (left > right)
             return defaultValue;
 
         if (left == right)
-            return rangeValue[node] = arr[left];
+            return tree[node] = arr[left];
 
         int mid = left + (right - left) / 2;
-        T leftSum = _init<U>(arr, left, mid, node * 2);
-        T rightSum = _init<U>(arr, mid + 1, right, node * 2 + 1);
+        T leftSum = buildSub<U>(arr, left, mid, node * 2);
+        T rightSum = buildSub<U>(arr, mid + 1, right, node * 2 + 1);
 
-        return rangeValue[node] = mergeOp(leftSum, rightSum);
+        return tree[node] = mergeOp(leftSum, rightSum);
     }
 
-    T _querySub(int left, int right, int node, int nodeLeft, int nodeRight) {
+    T querySub(int left, int right, int node, int nodeLeft, int nodeRight) {
         if (right < nodeLeft || nodeRight < left)
             return defaultValue;
 
         if (left <= nodeLeft && nodeRight <= right)
-            return rangeValue[node];
+            return tree[node];
 
         int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-        return mergeOp(_querySub(left, right, node * 2, nodeLeft, mid),
-                       _querySub(left, right, node * 2 + 1, mid + 1, nodeRight));
-    }
-    // inclusive
-    T query(int left, int right) {
-        return _querySub(left, right, 1, 0, n - 1);
+        return mergeOp(querySub(left, right, node * 2, nodeLeft, mid),
+                       querySub(left, right, node * 2 + 1, mid + 1, nodeRight));
     }
 
-    T _updateSub(int index, T newValue, int node, int nodeLeft, int nodeRight) {
+    T updateSub(int index, T newValue, int node, int nodeLeft, int nodeRight) {
         if (index < nodeLeft || nodeRight < index)
-            return rangeValue[node];
+            return tree[node];
 
         if (nodeLeft == nodeRight)
-            return rangeValue[node] = newValue;
+            return tree[node] = newValue;
 
         int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-        return rangeValue[node] = mergeOp(_updateSub(index, newValue, node * 2, nodeLeft, mid),
-                                          _updateSub(index, newValue, node * 2 + 1, mid + 1, nodeRight));
-    }
-    T update(int index, T newValue) {
-        return _updateSub(index, newValue, 1, 0, n - 1);
+        return tree[node] = mergeOp(updateSub(index, newValue, node * 2, nodeLeft, mid),
+                                    updateSub(index, newValue, node * 2 + 1, mid + 1, nodeRight));
     }
 
-    T _updateRangeSub(int left, int right, T newValue, int node, int nodeLeft, int nodeRight) {
+    T updateRangeSub(int left, int right, T newValue, int node, int nodeLeft, int nodeRight) {
         if (right < nodeLeft || nodeRight < left)
-            return rangeValue[node];
+            return tree[node];
 
         if (nodeLeft == nodeRight)
-            return rangeValue[node] = newValue;
+            return tree[node] = newValue;
 
         int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
-        return rangeValue[node] = mergeOp(_updateRangeSub(left, right, newValue, node * 2, nodeLeft, mid),
-                                          _updateRangeSub(left, right, newValue, node * 2 + 1, mid + 1, nodeRight));
-    }
-    // inclusive
-    T updateRange(int left, int right, T newValue) {
-        return _updateRangeSub(left, right, newValue, 1, 0, n - 1);
+        return tree[node] = mergeOp(updateRangeSub(left, right, newValue, node * 2, nodeLeft, mid),
+                                    updateRangeSub(left, right, newValue, node * 2 + 1, mid + 1, nodeRight));
     }
 };
 
@@ -189,13 +144,17 @@ SegmentTree<T, BinOp> makeSegmentTree(int size, BinOp op, T dfltValue = T()) {
 }
 
 template <typename T, typename BinOp>
-SegmentTree<T, BinOp> makeSegmentTree(const vector<T>& arr, BinOp op, T dfltValue = T()) {
-    return std::move(SegmentTree<T, BinOp>(arr, op, dfltValue));
+SegmentTree<T, BinOp> makeSegmentTree(const vector<T>& v, BinOp op, T dfltValue = T()) {
+    auto segTree = SegmentTree<T, BinOp>((int)v.size(), op, dfltValue);
+    segTree.build(v, 0, (int)v.size() - 1);
+    return std::move(segTree);
 }
 
 template <typename T, typename BinOp>
 SegmentTree<T, BinOp> makeSegmentTree(const T arr[], int size, BinOp op, T dfltValue = T()) {
-    return std::move(SegmentTree<T, BinOp>(arr, size, op, dfltValue));
+    auto segTree = SegmentTree<T, BinOp>(size, op, dfltValue);
+    segTree.build(arr, 0, size - 1);
+    return std::move(segTree);
 }
 
 /* example
@@ -216,35 +175,21 @@ struct RMQ {
     vector<int> valueId;
 
     RMQ(int size) {
-        _init(size);
+        init(size);
     }
 
     RMQ(int v[], int size) {
-        _init(size);
+        init(size);
         
         for (int i = 0; i < size; i++)
             update(i, v[i]);
     }
 
     RMQ(const vector<int>& v) {
-        _init((int)v.size());
+        init((int)v.size());
 
         for (int i = 0; i < (int)v.size(); i++)
             update(i, v[i]);
-    }
-
-    void _init(int size) {
-        N = 1;
-        while (N <= size)
-            N <<= 1;
-
-        value.resize(N * 2);
-        valueId.resize(N * 2);
-        for (int i = 0; i < N; i++)
-            valueId[N + i] = i;
-
-        for (int i = 0; i < size; i++)
-            update(i, INT_MAX);
     }
 
     void update(int x, int val) {
@@ -283,5 +228,20 @@ struct RMQ {
             right >>= 1;
         }
         return ret;
+    }
+
+private:
+    void init(int size) {
+        N = 1;
+        while (N <= size)
+            N <<= 1;
+
+        value.resize(N * 2);
+        valueId.resize(N * 2);
+        for (int i = 0; i < N; i++)
+            valueId[N + i] = i;
+
+        for (int i = 0; i < size; i++)
+            update(i, INT_MAX);
     }
 };
