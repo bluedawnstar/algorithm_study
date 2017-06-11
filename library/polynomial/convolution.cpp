0835222e3275cffb3617ffaf84b5461a632dbcf7
youngman <youngman.ro@gmail.com>
2017-06-11 13:19:50 +0900

    adding 'reverseH' option to interative convolution functions

--- library/polynomial/convolution.cpp
+++ library/polynomial/convolution.cpp
@@ -2,19 +2,29 @@ using namespace std;
 
 #include "fft.h"
 
-// y(n) = SUM x(t) * h(n - t)
 template <typename T = int>
-vector<T> convolution(const vector<T>& x, const vector<T>& h) {
+vector<T> convolution(const vector<T>& x, const vector<T>& h, bool reverseH = true) {
     int xN = (int)x.size();
     int hN = (int)h.size();
 
     vector<T> y(xN + hN - 1);
 
     int n = min(xN, hN);
-    for (int i = 0; i < (int)y.size(); i++) {
-        for (int L = i - hN + 1, R = hN - 1; R >= 0; L++, R--) {
-            if (L >= 0 && L < xN && R < hN)
-                y[i] += x[L] * h[R];
+    if (reverseH) {
+        // y(n) = SUM x(t) * h(n - t)
+        for (int i = 0; i < (int)y.size(); i++) {
+            for (int L = i - hN + 1, R = hN - 1; R >= 0; L++, R--) {
+                if (L >= 0 && L < xN)
+                    y[i] += x[L] * h[R];
+            }
+        }
+    } else {
+        // y(n) = SUM x(t) * h(t)
+        for (int i = 0; i < (int)y.size(); i++) {
+            for (int L = i - hN + 1, R = 0; R < hN; L++, R++) {
+                if (L >= 0 && L < xN)
+                    y[i] += x[L] * h[R];
+            }
         }
     }
 
@@ -64,18 +74,28 @@ vector<T> convolutionFFT(const vector<T>& x, const vector<T>& h, bool reverseH =
 
 #define SCALE   32768
 
-// y(n) = SUM x(t) * h(n - t)
-vector<int> convolutionMod(const vector<int>& x, const vector<int>& h, int MOD) {
+vector<int> convolutionMod(const vector<int>& x, const vector<int>& h, int MOD, bool reverseH = true) {
     int xN = (int)x.size();
     int hN = (int)h.size();
 
     vector<int> y(xN + hN - 1);
 
     int n = min(xN, hN);
-    for (int i = 0; i < (int)y.size(); i++) {
-        for (int L = i - hN + 1, R = hN - 1; R >= 0; L++, R--) {
-            if (L >= 0 && L < xN && R < hN)
-                y[i] = ((long long)y[i] + (long long)x[L] * h[R]) % MOD;
+    if (reverseH) {
+        // y(n) = SUM x(t) * h(n - t)
+        for (int i = 0; i < (int)y.size(); i++) {
+            for (int L = i - hN + 1, R = hN - 1; R >= 0; L++, R--) {
+                if (L >= 0 && L < xN)
+                    y[i] = ((long long)y[i] + (long long)x[L] * h[R]) % MOD;
+            }
+        }
+    } else {
+        // y(n) = SUM x(t) * h(t)
+        for (int i = 0; i < (int)y.size(); i++) {
+            for (int L = i - hN + 1, R = 0; R < hN; L++, R++) {
+                if (L >= 0 && L < xN)
+                    y[i] = ((long long)y[i] + (long long)x[L] * h[R]) % MOD;
+            }
         }
     }
 
@@ -164,12 +184,17 @@ void testConvolution() {
     vector<int> v2 = convolutionFFT(x, h);
     cout << x << " * " << h << " : ";
     cout << v2 << endl;
-    assert(operator ==(v2, vector<int>{ 1, 4, 7, 6 }));
+    assert(v == v2);
+
+    v = convolution(x, h, false);
+    cout << x << " * " << h << " : ";
+    cout << v << endl;
+    assert(operator ==(v, vector<int>{ 2, 5, 8, 3 }));
 
     v2 = convolutionFFT(x, h, false);
     cout << x << " * " << h << " : ";
     cout << v2 << endl;
-    assert(operator ==(v2, vector<int>{ 2, 5, 8, 3 }));
+    assert(v == v2);
 
     v = convolutionMod(x, h, MOD);
     cout << x << " * " << h << " : ";
@@ -179,12 +204,17 @@ void testConvolution() {
     v2 = convolutionFFTMod(x, h, MOD);
     cout << x << " * " << h << " : ";
     cout << v2 << endl;
-    assert(operator ==(v2, vector<int>{ 1, 4, 7, 6 }));
+    assert(v == v2);
+
+    v = convolutionMod(x, h, MOD, false);
+    cout << x << " * " << h << " : ";
+    cout << v << endl;
+    assert(operator ==(v, vector<int>{ 2, 5, 8, 3 }));
 
     v2 = convolutionFFTMod(x, h, MOD, false);
     cout << x << " * " << h << " : ";
     cout << v2 << endl;
-    assert(operator ==(v2, vector<int>{ 2, 5, 8, 3 }));
+    assert(v == v2);
 
     cout << "OK!" << endl;
 }
