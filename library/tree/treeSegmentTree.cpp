34cc8ec19c4df4ce917c67e4e242450764cd9872
youngman <youngman.ro@gmail.com>
2017-06-13 06:09:23 +0900

    changing project configuration

--- library/tree/treeSegmentTree.cpp
+++ library/tree/treeSegmentTree.cpp
@@ -18,7 +18,7 @@ using namespace std;
 #include "../common/profile.h"
 
 void testSegmentTree() {
-    //return; //TODO: if you want to test a split function, make this line a comment.
+    return; //TODO: if you want to test a split function, make this line a comment.
 
     cout << "-- Segment Tree ----------------------------------------" << endl;
 
