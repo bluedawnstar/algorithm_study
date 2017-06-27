f735d69260891e8c253ee0c908a5feaadcffb3e9
youngman <youngman.ro@gmail.com>
2017-06-28 03:29:57 +0900

    Revert "Revert "configuration was changed""
    This reverts commit d08a51d76aa4a246a4e916422dbc013916ba786c.

--- library/tree/treapRangeQuery.cpp
+++ library/tree/treapRangeQuery.cpp
@@ -1,6 +1,8 @@
+#include <cassert>
+
 using namespace std;
 
-#include "splayTree.h"
+#include "treap.h"
 
 /////////// For Testing ///////////////////////////////////////////////////////
 
@@ -10,91 +12,63 @@ using namespace std;
 #include <numeric>
 #include <set>
 #include <vector>
-#include <bitset>
-#include <unordered_set>
 #include <iostream>
 #include "../common/iostreamhelper.h"
 #include "../common/profile.h"
 
 #include "redBlackTree.h"
+#include "splayTree.h"
 #include "segmentTreeLazy.h"
 
 void checkSearch(RBTree<int>& rbt, vector<int>& in);
 void checkIndex(RBTree<int>& rbt, vector<int>& in);
 
 template <typename T, typename MergeOp, typename BlockOp>
-void checkSearch(SplayTreeRangeQuery<T, MergeOp, BlockOp>& spt, vector<int>& in) {
+void checkIndex(TreapRangeQuery<T, MergeOp, BlockOp>& tr, vector<int>& in) {
+    assert((tr.tree != nullptr ? tr.tree->cnt : 0) == tr.size());
+    assert(tr.size() == (int)in.size());
     for (int i = 0; i < (int)in.size(); i++) {
-        int x = in[i];
-        assert(spt.find(x)->value == x);
+        assert(tr[i]->value == in[i]);
+        assert(tr.indexOf(tr[i]) == i);
     }
 }
 
-template <typename T, typename MergeOp, typename BlockOp>
-void checkIndex(SplayTreeRangeQuery<T, MergeOp, BlockOp>& spt, vector<int>& in) {
-    assert((spt.tree != nullptr ? spt.tree->cnt : 0) == spt.size());
-    assert(spt.size() == (int)in.size());
-    for (int i = 0; i < (int)in.size(); i++) {
-        assert(spt[i]->value == in[i]);
-        assert(spt.indexOf(spt[i]) == i);
-    }
-}
+void testTreapRangeQuery() {
+    //return; //TODO: if you want to test a split function, make this line a comment.
 
-void testSplayRangeQuery() {
-    return; //TODO: if you want to test a split function, make this line a comment.
+    cout << "--- Treap with Range Query ----------------------------------" << endl;
 
-    cout << "--- Splay Tree with Range Query ----------------------------------" << endl;
-
+    srand((unsigned)time(nullptr));
     {
         RBTree<int> rbt;
-        auto spt = makeSplayTreeRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
+        auto tr = makeTreapRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
 
         vector<int> in(1000);
         iota(in.begin(), in.end(), 0);
 
         {
             vector<int> t(in);
-            random_shuffle(t.begin(), t.end());
             for (int i = 0; i < (int)in.size(); i++) {
-                auto p1 = rbt.insert(t[i]);
-                if (!p1.second)
+                auto p = tr.insert(i, t[i]);
+                if (!p)
                     cerr << "It'll never be shown!" << endl;
-                assert(p1.first->value == t[i] && p1.second);
-
-                auto p2 = spt.insert(t[i]);
-                if (!p2.second)
-                    cerr << "It'll never be shown!" << endl;
-                assert(p2.first->value == t[i] && p2.second);
+                assert(p && p->value == t[i]);
             }
-            checkSearch(rbt, in);
-            checkIndex(rbt, in);
-            checkSearch(spt, in);
-            checkIndex(spt, in);
+            checkIndex(tr, t);
         }
         {
-            vector<int> t(in), org(in);
+            vector<int> t(in);
 
-            random_shuffle(t.begin(), t.end());
-            while (!rbt.empty()) {
-                int x = t.back();
-                t.pop_back();
-                org.erase(find(org.begin(), org.end(), x));
+            for (int i = 0; i < (int)in.size(); i++) {
+                int idx = rand() % (int)t.size();
+                t.erase(t.begin() + idx);
 
-                bool b1 = rbt.erase(x);
-                if (!b1)
+                bool b = tr.erase(idx);
+                if (!b)
                     cerr << "It'll never be shown!" << endl;
-                assert(b1);
-                assert(rbt.size() == (int)t.size());
-                checkSearch(rbt, org);
-                checkIndex(rbt, org);
-
-                bool b2 = spt.erase(x);
-                if (!b2)
-                    cerr << "It'll never be shown!" << endl;
-                assert(b2);
-                assert(spt.size() == (int)t.size());
-                checkSearch(spt, org);
-                checkIndex(spt, org);
+                assert(b);
+                assert(tr.size() == (int)t.size());
+                checkIndex(tr, t);
             }
         }
     }
@@ -102,20 +76,34 @@ void testSplayRangeQuery() {
     {
         int N = 10000;
         auto segt = makeSegmentTreeLazy(N, [](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
-        auto spt = makeSplayTreeRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
+        auto tr = makeTreapRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
 
-        spt.build(N);
+        for (int i = 0; i < N; i++)
+            tr.insert(i, 0);
+
+        assert(segt.query(0, N - 1) == tr.query(0, N - 1));
+        assert(segt.query(1, N - 1) == tr.query(1, N - 1));
+        assert(segt.query(0, N - 2) == tr.query(0, N - 2));
+        assert(segt.query(1, N - 2) == tr.query(1, N - 2));
+
+        segt.update(0, 1);
+        tr.update(0, 1);
+
+        assert(segt.query(0, N - 1) == tr.query(0, N - 1));
+        assert(segt.query(1, N - 1) == tr.query(1, N - 1));
+        assert(segt.query(0, N - 2) == tr.query(0, N - 2));
+        assert(segt.query(1, N - 2) == tr.query(1, N - 2));
 
         for (int i = 0; i < N; i++) {
             int x = rand() % 100;
             segt.update(i, x);
-            spt.update(i, x);
+            tr.update(i, x);
         }
 
-        assert(segt.query(0, N - 1) == spt.query(0, N - 1));
-        assert(segt.query(1, N - 1) == spt.query(1, N - 1));
-        assert(segt.query(0, N - 2) == spt.query(0, N - 2));
-        assert(segt.query(1, N - 2) == spt.query(1, N - 2));
+        assert(segt.query(0, N - 1) == tr.query(0, N - 1));
+        assert(segt.query(1, N - 1) == tr.query(1, N - 1));
+        assert(segt.query(0, N - 2) == tr.query(0, N - 2));
+        assert(segt.query(1, N - 2) == tr.query(1, N - 2));
 
         for (int i = 0; i < N * 4; i++) {
             int left, right, value;
@@ -125,12 +113,12 @@ void testSplayRangeQuery() {
                 left = rand() % N;
                 value = rand() % 100;
                 segt.update(left, value);
-                spt.update(left, value);
-                assert(segt.query(left, left) == spt.query(left));
-                assert(segt.query(0, N - 1) == spt.query(0, N - 1));
-                assert(segt.query(1, N - 1) == spt.query(1, N - 1));
-                assert(segt.query(0, N - 2) == spt.query(0, N - 2));
-                assert(segt.query(1, N - 2) == spt.query(1, N - 2));
+                tr.update(left, value);
+                assert(segt.query(left, left) == tr.query(left));
+                assert(segt.query(0, N - 1) == tr.query(0, N - 1));
+                assert(segt.query(1, N - 1) == tr.query(1, N - 1));
+                assert(segt.query(0, N - 2) == tr.query(0, N - 2));
+                assert(segt.query(1, N - 2) == tr.query(1, N - 2));
                 break;
             case 1:
                 left = rand() % N;
@@ -139,18 +127,18 @@ void testSplayRangeQuery() {
                     swap(left, right);
                 value = rand() % 100;
                 segt.updateRange(left, right, value);
-                spt.update(left, right, value);
-                assert(segt.query(left, right) == spt.query(left, right));
-                assert(segt.query(0, N - 1) == spt.query(0, N - 1));
-                assert(segt.query(1, N - 1) == spt.query(1, N - 1));
-                assert(segt.query(0, N - 2) == spt.query(0, N - 2));
-                assert(segt.query(1, N - 2) == spt.query(1, N - 2));
+                tr.update(left, right, value);
+                assert(segt.query(left, right) == tr.query(left, right));
+                assert(segt.query(0, N - 1) == tr.query(0, N - 1));
+                assert(segt.query(1, N - 1) == tr.query(1, N - 1));
+                assert(segt.query(0, N - 2) == tr.query(0, N - 2));
+                assert(segt.query(1, N - 2) == tr.query(1, N - 2));
                 break;
             case 2:
             {
                 left = rand() % N;
                 int v1 = segt.query(left, left);
-                int v2 = spt.query(left);
+                int v2 = tr.query(left);
                 if (v1 != v2)
                     cerr << "Mismatched! : " << v1 << ", " << v2 << endl;
                 assert(v1 == v2);
@@ -163,10 +151,10 @@ void testSplayRangeQuery() {
                 if (left > right)
                     swap(left, right);
                 int v1 = segt.query(left, right);
-                int v2 = spt.query(left, right);
+                int v2 = tr.query(left, right);
                 if (v1 != v2) {
                     cerr << "Mismatched! : " << v1 << ", " << v2 << endl;
-                    spt.query(left, right);
+                    tr.query(left, right);
                 }
                 assert(v1 == v2);
                 break;
@@ -179,7 +167,7 @@ void testSplayRangeQuery() {
     {
         const int N = 10000;
 
-        auto spt = makeSplayTreeRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
+        auto tr = makeTreapRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
         vector<int> vec;
 
         vector<int> possible;
@@ -193,9 +181,10 @@ void testSplayRangeQuery() {
                 if (!possible.empty()) {
                     int x = possible.front();
                     possible.erase(possible.begin());
-                    vec.insert(lower_bound(vec.begin(), vec.end(), x), x);
-                    spt.insert(x);
-                    assert(accumulate(vec.begin(), vec.end(), 0) == spt.query(0, spt.size() - 1));
+                    int idx = lower_bound(vec.begin(), vec.end(), x) - vec.begin();
+                    vec.insert(vec.begin() + idx, x);
+                    tr.insert(idx, x);
+                    assert(accumulate(vec.begin(), vec.end(), 0) == tr.query(0, tr.size() - 1));
                 }
                 break;
             case 1: // erase
@@ -204,22 +193,22 @@ void testSplayRangeQuery() {
                     int x = vec[idx];
                     possible.push_back(x);
                     vec.erase(vec.begin() + idx);
-                    spt.erase(x);
+                    tr.erase(idx);
 
                     int v1 = accumulate(vec.begin(), vec.end(), 0);
-                    int v2 = spt.query(0, spt.size() - 1);
+                    int v2 = tr.query(0, tr.size() - 1);
                     if (v1 != v2) {
                         accumulate(vec.begin(), vec.end(), 0);
-                        spt.query(0, spt.size() - 1);
+                        tr.query(0, tr.size() - 1);
                     }
-                    assert(accumulate(vec.begin(), vec.end(), 0) == spt.query(0, spt.size() - 1));
+                    assert(accumulate(vec.begin(), vec.end(), 0) == tr.query(0, tr.size() - 1));
                 }
                 break;
             case 2: // query
                 if (!vec.empty()) {
                     int idx = rand() % (int)vec.size();
-                    assert(vec[idx] == spt.query(idx));
-                    assert(accumulate(vec.begin(), vec.end(), 0) == spt.query(0, spt.size() - 1));
+                    assert(vec[idx] == tr.query(idx));
+                    assert(accumulate(vec.begin(), vec.end(), 0) == tr.query(0, tr.size() - 1));
                 }
                 break;
             case 3: // query
@@ -230,13 +219,13 @@ void testSplayRangeQuery() {
                         swap(left, right);
 
                     int v1 = accumulate(vec.begin() + left, vec.begin() + right + 1, 0);
-                    int v2 = spt.query(left, right);
+                    int v2 = tr.query(left, right);
                     if (v1 != v2) {
                         accumulate(vec.begin() + left, vec.begin() + right + 1, 0);
-                        spt.query(left, right);
+                        tr.query(left, right);
                     }
                     assert(v1 == v2);
-                    assert(accumulate(vec.begin(), vec.end(), 0) == spt.query(0, spt.size() - 1));
+                    assert(accumulate(vec.begin(), vec.end(), 0) == tr.query(0, tr.size() - 1));
                 }
                 break;
             }
@@ -244,18 +233,22 @@ void testSplayRangeQuery() {
     }
     cout << "OK!" << endl;
 
-    cout << "*** Speed test SegmentTreeLazy vs SplayTreeRangeQuery ***" << endl;
+    cout << "*** Speed test SegmentTreeLazy vs SplayTreeRangeQuery vs TreapRangeQuery ***" << endl;
     {
-        int N = 100000;
+        int N = 1000000;
         auto segt = makeSegmentTreeLazy(N, [](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
         auto spt = makeSplayTreeRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
+        auto tr = makeTreapRangeQuery([](int a, int b) { return a + b; }, [](int x, int n) { return x * n; }, 0);
 
         spt.build(N);
+        for (int i = 0; i < N; i++)
+            tr.insert(i, 0);
 
         for (int i = 0; i < N; i++) {
             int x = rand() % 100;
             segt.update(i, x);
             spt.update(i, x);
+            tr.update(i, x);
         }
 
         int tN = 1000000;
@@ -295,6 +288,19 @@ void testSplayRangeQuery() {
             }
         }
         PROFILE_STOP(1);
+
+        PROFILE_START(2);
+        for (int i = 0; i < (int)in.size(); i++) {
+            if (!get<0>(in[i])) {
+                tr.update(get<1>(in[i]), get<2>(in[i]), get<3>(in[i]));
+                if (tr.count <= 0)
+                    cerr << "It'll never be shown!" << endl;
+            } else {
+                if (tr.query(get<1>(in[i]), get<2>(in[i])) < 0)
+                    cerr << "It'll never be shown!" << endl;
+            }
+        }
+        PROFILE_STOP(2);
     }
     cout << "OK!" << endl;
 }
