abef8f1d1ab2249f6d5a431656215cdb04148fdc
youngman <youngman.ro@gmail.com>
2017-07-29 16:51:01 +0900

    adding a math project

---
+++ library/math/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+#define TEST(ModuleName)    extern void test##ModuleName(void); \
+                            test##ModuleName()
+
+int main(void) {
+    TEST(Fibonacci);
+}
