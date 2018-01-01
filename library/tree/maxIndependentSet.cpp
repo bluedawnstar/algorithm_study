2ced87bdd5f6c7980848bc2d433213a15bfc3a80
youngman <youngman.ro@gmail.com>
2018-01-01 20:17:30 +0900

    [Add] maximum independent set in tree

---
+++ library/tree/maxIndependentSet.cpp
@@ -0,0 +1,28 @@
+#include <climits>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#include "maxIndependentSet.h"
+
+/////////// For Testing ///////////////////////////////////////////////////////
+
+#include <cassert>
+#include <string>
+#include <iostream>
+#include "../common/iostreamhelper.h"
+#include "../common/profile.h"
+
+
+void testMaxIndependentSet() {
+    //return; //TODO: if you want to test a split function, make this line a comment.
+
+    cout << "-- Max Independent Set -----------------------" << endl;
+
+    //...
+
+    cout << "OK" << endl;
+}
---
+++ library/tree/maxIndependentSet.h
@@ -0,0 +1,48 @@
+#pragma once
+
+template <typename T>
+struct MaxWeightedIndependentSet {
+    static const T INF = T(0x3f3f3f3f);
+
+    static T getMaxWeightedSum(const vector<vector<int>>& edges, const vector<T>& vertexWeights, int root) {
+        vector<vector<T>> dp(edges.size(), vector<T>(2));
+        dfsMaxWeightedSum(edges, vertexWeights, dp, root, -1);
+        return max(dp[root][false], dp[root][true]);
+    }
+
+private:
+    static void dfsMaxWeightedSum(const vector<vector<int>>& edges, const vector<T>& vertexWeights,
+                                 vector<vector<T>>& dp, int u, int parent) {
+        dp[u][true] = vertexWeights[u];
+        dp[u][false] = 0;
+        for (int v : edges[u]) {
+            if (v == parent)
+                continue;
+
+            dp[u][true] += dp[v][false];
+            dp[u][false] += max(dp[v][false], dp[v][true]);
+        }
+    }
+};
+
+struct MaxIndependentSet {
+    static int getLargestSize(const vector<vector<int>>& edges, int root) {
+        vector<vector<int>> dp(edges.size(), vector<int>(2));
+        dfsLargestSize(edges, dp, root, -1);
+        return max(dp[root][false], dp[root][true]);
+
+    }
+
+private:
+    static void dfsLargestSize(const vector<vector<int>>& edges, vector<vector<int>>& dp, int u, int parent) {
+        dp[u][true] = 1;
+        dp[u][false] = 0;
+        for (int v : edges[u]) {
+            if (v == parent)
+                continue;
+
+            dp[u][true] += dp[v][false];
+            dp[u][false] += max(dp[v][false], dp[v][true]);
+        }
+    }
+};
