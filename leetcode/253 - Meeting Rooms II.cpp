963a13c590ece05cc9ce85712555b681e7ceaa66
youngman <youngman.ro@gmail.com>
2018-01-03 23:54:22 +0900

    [Add] leetcode solutions

---
+++ leetcode/253 - Meeting Rooms II.cpp
@@ -0,0 +1,32 @@
+/**
+* Definition for an interval.
+* struct Interval {
+*     int start;
+*     int end;
+*     Interval() : start(0), end(0) {}
+*     Interval(int s, int e) : start(s), end(e) {}
+* };
+*/
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const Interval& l, const Interval& r) {
+            if (l.end != r.end)
+                return l.end < r.end;
+            return l.start < r.start;
+        });
+
+        multiset<int> S;
+
+        int ans = 0;
+        for (int i = 0; i < (int)intervals.size(); i++) {
+            if (S.empty() || intervals[i].start < *S.begin())
+                ans = max(ans, (int)S.size() + 1);
+            else
+                S.erase(--S.lower_bound(intervals[i].start + 1));
+            S.insert(intervals[i].end);
+        }
+
+        return ans;
+    }
+};
