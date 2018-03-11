920bd2e03915bc660ed18adeea5e27dece3740cf
youngman <youngman.ro@gmail.com>
2018-03-11 15:21:57 +0900

    [Add] test code for random access sparse table

--- library/misc/randomAccessSparseTable.cpp
+++ library/misc/randomAccessSparseTable.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <vector>
 #include <algorithm>
 
@@ -16,30 +17,69 @@ using namespace std;
 #include "../common/iostreamhelper.h"
 #include "../common/profile.h"
 
+static int findFirst(const vector<int>& A, int start, int R) {
+    while (start > R)
+        start = A[start];
+    return start;
+}
+
+static int findLast(const vector<int>& A, int start, int R) {
+    while (A[start] > R)
+        start = A[start];
+    return start;
+}
+
 void testRandomAccessSparseTable() {
-    return; //TODO: if you want to test functions of this file, make this line a comment.
+    //return; //TODO: if you want to test functions of this file, make this line a comment.
 
     cout << "--- Random Access Sparse Table -------------" << endl;
-    int N = 100;
-    vector<int> in(N);
-    for (int i = 0; i < N; i++)
-        in[i] = i;
-    random_shuffle(in.begin(), in.end());
+    {
+        int N = 100;
+        vector<int> in(N);
+        for (int i = 0; i < N; i++)
+            in[i] = i;
+        random_shuffle(in.begin(), in.end());
 
-    RandomAccessSparseTable rast(N);
-    rast.add(in[0], -1);
-    for (int i = 1; i < N; i++)
-        rast.add(in[i], in[i - 1]);
+        RandomAccessSparseTable rast(N);
+        rast.add(in[0], -1);
+        for (int i = 1; i < N; i++)
+            rast.add(in[i], in[i - 1]);
 
-    //cout << in << endl;
-    for (int i = 0; i < N; i++) {
-        for (int j = i; j >= 0; j--) {
-            int ans = rast.query(in[i], i - j);
-            int gt = in[j];
-            if (ans != gt) {
-                cout << "Wrong at (" << i << ", " << j << ") : gt = " << gt << ", ans = " << ans << endl;
+        //cout << in << endl;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j >= 0; j--) {
+                int ans = rast.query(in[i], i - j);
+                int gt = in[j];
+                if (ans != gt) {
+                    cout << "Wrong at (" << i << ", " << j << ") : gt = " << gt << ", ans = " << ans << endl;
+                }
+                assert(ans == gt);
             }
-            assert(ans == gt);
+        }
+    }
+    {
+        static const int T = 500;
+        int N = 10000;
+        vector<int> in(N);
+        in[0] = -1;
+        for (int i = 1; i < N; i++)
+            in[i] = rand() % i;
+
+        RandomAccessSparseTable rast(N);
+        rast.build(in);
+
+        for (int i = N - 1; i >= 0; i--) {
+            int ans1 = rast.findFirst(i, [](int x) {
+                return x <= T;
+            });
+            int gt1 = findFirst(in, i, T);
+            assert(gt1 == ans1);
+
+            int ans2 = rast.findLast(i, [](int x) {
+                return x > T;
+            });
+            int gt2 = findLast(in, i, T);
+            assert(gt2 == ans2);
         }
     }
 
--- library/misc/randomAccessSparseTable.h
+++ library/misc/randomAccessSparseTable.h
@@ -19,8 +19,30 @@ struct RandomAccessSparseTable {
             mP[i].resize(mN);
     }
 
+    // for batch building
+    // O(NlogN)
+    void build() {
+        for (int i = 1; i < mLogN; i++) {
+            for (int j = 0; j < mN; j++) {
+                int pp = mP[i - 1][j];
+                mP[i][j] = pp < 0 ? pp : mP[i - 1][pp];
+            }
+        }
+    }
+
+    void build(const int A[], int n) {
+        copy(A, A + n, mP[0].begin());
+        build();
+    }
+
+    void build(const vector<int>& A) {
+        copy(A.begin(), A.end(), mP[0].begin());
+        build();
+    }
+
+    // for incremental building
     // x = index, next = x's next index (pointer)
-    // 0 <= x, next <= N
+    // 0 <= x < N, -1 <= next < N
     void add(int x, int next) {
         mP[0][x] = next;
         for (int i = 1; i < mLogN; i++) {
@@ -29,6 +51,8 @@ struct RandomAccessSparseTable {
         }
     }
 
+    // get next[next[next[...[x]]]]
+    // O(logD)
     int query(int x, int dist) const {
         if (dist <= 0)
             return x;
@@ -40,5 +64,61 @@ struct RandomAccessSparseTable {
         }
 
         return x;
+    }
+
+    // get last value from start in range [start, rightLimit]
+    int findLast(int start, int rightLimit) const {
+        int res = start;
+        for (int i = mLogN - 1; i >= 0; i--) {
+            int next = mP[i][res];
+            if (0 <= next && next <= rightLimit)
+                res = next;
+        }
+
+        return res;
+    }
+
+    // find last position where f(x) is true
+    //   f(x): OOOOOOOOOOOxxxxxxxx
+    //         S         ^
+    int findLast(int start, const function<bool(int)>& f) const {
+        int res = start;
+        for (int i = mLogN - 1; i >= 0; i--) {
+            int next = mP[i][res];
+            if (0 <= next && f(next))
+                res = next;
+        }
+
+        return res;
+    }
+
+    // get last value from start in range [leftLimit, N)
+    int findFirst(int start, int leftLimit) const {
+        if (start >= leftLimit)
+            return start;
+
+        int res = start;
+        for (int i = mLogN - 1; i >= 0; i--) {
+            int next = mP[i][res];
+            if (0 <= next && next < leftLimit)
+                res = next;
+        }
+        return mP[0][res] >= leftLimit ? mP[0][res] : res;
+    }
+
+    // find first position where f(x) is true in [start, N)
+    //   f(x): xxxxxxxxxxxOOOOOOOO
+    //         S          ^
+    int findFirst(int start, const function<bool(int)>& f) const {
+        if (f(start))
+            return start;
+
+        int res = start;
+        for (int i = mLogN - 1; i >= 0; i--) {
+            int next = mP[i][res];
+            if (0 <= next && !f(next))
+                res = next;
+        }
+        return f(mP[0][res]) ? mP[0][res] : res;
     }
 };
