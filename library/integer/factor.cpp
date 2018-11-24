db8e3a3bb4acec46c44ae4c686eba6a1a84f6bb5
youngman <youngman.ro@gmail.com>
2018-11-25 04:27:03 +0900

    [Add] divisor counter

--- library/integer/factor.cpp
+++ library/integer/factor.cpp
@@ -14,6 +14,7 @@ using namespace std;
 #include <cassert>
 #include <iostream>
 #include "../common/iostreamhelper.h"
+#include "../common/profile.h"
 
 #define NN  100
 
@@ -21,42 +22,71 @@ using namespace std;
 #include "primeNumberEratosthenes.h"
 
 void testFactor() {
-    return; //TODO: if you want to test, make this line a comment.
+    //return; //TODO: if you want to test, make this line a comment.
 
     cout << "--- getFactors(int x) -------------------------" << endl;
-    for (int i = 0; i <= 100; i++) {
-        vector<int> v = getFactors(i);
-        cout << i << ": " << v << endl;
-    }
-
-    cout << "--- getFactors(vector<pair<int,int>>&) -------------------------" << endl;
-    for (int i = 1; i <= 100; i++) {
-        PrimeFactors pf(i);
-        vector<int> factors = pf.getFactors();
-        {
+    {
+        for (int i = 0; i <= 100; i++) {
             vector<int> v = getFactors(i);
-            assert(factors == v);
+            cout << i << ": " << v << endl;
         }
-        cout << i << ": " << factors << endl;
     }
-
+    cout << "--- getFactors(vector<pair<int,int>>&) -------------------------" << endl;
+    {
+        for (int i = 1; i <= 100; i++) {
+            PrimeFactors pf(i);
+            vector<int> factors = pf.getFactors();
+            {
+                vector<int> v = getFactors(i);
+                assert(factors == v);
+            }
+            cout << i << ": " << factors << endl;
+        }
+    }
     cout << "--- getAllFactorCount() -------------------------" << endl;
-    MinFactors minFactors(NN);
-    vector<int> factorCount = minFactors.getAllFactorCount();
-    for (int i = 0; i <= NN; i++) {
-        cout << "(" << i << ": " << factorCount[i] << "), ";
+    {
+        MinFactors minFactors(NN);
+        vector<int> factorCount = minFactors.getAllFactorCount();
+        for (int i = 0; i <= NN; i++) {
+            cout << "(" << i << ": " << factorCount[i] << "), ";
+        }
+        cout << endl;
+
+        vector<int> factorCount2 = getAllFactorCountSimple(NN);
+        for (int i = 0; i <= NN; i++) {
+            cout << "(" << i << ": " << factorCount2[i] << "), ";
+        }
+        cout << endl;
+
+        assert(factorCount == factorCount2);
     }
-    cout << endl;
-
-    cout << "--- getAllFactorCountSimple() -------------------------" << endl;
-    vector<int> factorCount2 = getAllFactorCountSimple(NN);
-    for (int i = 0; i <= NN; i++) {
-        cout << "(" << i << ": " << factorCount2[i] << "), ";
+    cout << "--- DivisorCounter ------------" << endl;
+    {
+        PROFILE_START(0);
+        DivisorCounter dc;
+        for (int T = 1000000000; T < 1000001000; T++) {
+            int gt = int(getFactors(T).size());
+            int ans = dc.count(T);
+            if (ans != gt)
+                cout << "Mismatched : " << ans << ", " << gt << endl;
+            assert(ans == gt);
+        }
+        PROFILE_STOP(0);
     }
-    cout << endl;
-
-    assert(factorCount == factorCount2);
-
+#ifndef _DEBUG
+    {
+        PROFILE_START(1);
+        DivisorCounter dc;
+        for (long long T = 1000000000000000; T < 1000000000000100; T++) {
+            int gt = int(getFactors(T).size());
+            int ans = dc.count(T);
+            if (ans != gt)
+                cout << "Mismatched : " << ans << ", " << gt << endl;
+            assert(ans == gt);
+        }
+        PROFILE_STOP(1);
+    }
+#endif
     cout << "--- performance test of getAllFactorCountXXX() --" << endl;
     {
         MinFactors minFactors(NN);
--- library/integer/factor.h
+++ library/integer/factor.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 // return factors of a number x
 // O(sqrt(N))
 template <typename T>
@@ -38,3 +37,95 @@ inline vector<int> getAllFactorCountSimple(int n) {
 
     return factorCount;
 }
+
+
+#include "primalityTest.h"
+
+// https://codeforces.com/blog/entry/22317
+struct DivisorCounter {
+    const int TABLE_SIZE = 1000000;
+
+    vector<bool> primeTest;
+    vector<int> primes;
+
+    DivisorCounter() {
+        eratosthenes();
+        eratosthenes2();
+    }
+
+    // O(N^(1/3))
+    template <typename T>
+    int count(T n) const {
+        int ans = 1;
+
+        for (auto p : primes) {
+            T ppp = T(p) * p * p;
+            if (ppp > n)
+                break;
+
+            int count = 1;
+            while (n % p == 0) {
+                n /= p;
+                count++;
+            }
+            ans *= count;
+        }
+
+        if (n > 1) {
+            if (isPrime(n))
+                ans *= 2;
+            else {
+                T sqrtN = T(sqrt(n));
+                if (sqrtN * sqrtN == n)
+                    ans *= 3;
+                else
+                    ans *= 4;
+            }
+        }
+
+        return ans;
+    }
+
+private:
+    template <typename T>
+    bool isPrime(T n) const {
+        if (n <= TABLE_SIZE)
+            return primeTest[int(n)];
+        
+        //TODO: optimize
+
+        return PrimalityTest<T>::isPrimeNumber(n);
+    }
+
+    // O(N*loglogN)
+    void eratosthenes() {
+        primeTest = vector<bool>(TABLE_SIZE + 1, true);
+        primeTest[0] = false;
+        primeTest[1] = false;
+
+        if (TABLE_SIZE >= 4) {
+            for (int j = 2 * 2; j <= TABLE_SIZE; j += 2)
+                primeTest[j] = false;
+        }
+
+        int root = int(sqrt(TABLE_SIZE));
+        for (int i = 3; i <= root; i += 2) {
+            if (primeTest[i]) {
+                for (int j = i * i; j >= 0 && j <= TABLE_SIZE; j += i)
+                    primeTest[j] = false;
+            }
+        }
+    }
+
+    // O(N)
+    void eratosthenes2() {
+        primes.clear();
+
+        if (TABLE_SIZE >= 2)
+            primes.push_back(2);
+
+        for (int i = 3; i <= TABLE_SIZE; i += 2)
+            if (primeTest[i])
+                primes.push_back(i);
+    }
+};
