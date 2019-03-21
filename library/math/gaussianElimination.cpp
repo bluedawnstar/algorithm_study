7125b19c3f15dacc9816e3775daa90cf5559b43a
youngman <youngman.ro@gmail.com>
2019-03-22 00:38:46 +0900

    [Add] general gausian elimination

--- library/math/gaussianElimination.cpp
+++ library/math/gaussianElimination.cpp
@@ -17,14 +17,51 @@ using namespace std;
 #include "../common/iostreamhelper.h"
 
 void testGaussianElimination() {
-    return; //TODO: if you want to test, make this line a comment.
+    //return; //TODO: if you want to test, make this line a comment.
 
     cout << "--- Gaussian Elimination -----------------------" << endl;
-    auto ans = gauss(Matrix<double>(vector<vector<double>>{ { 4, 2, -1 }, { 2, 4, 3 }, { -1, 3, 5 } }), vector<double>{ 5, 19, 20 });
-    cout << ans << endl;
-    assert(int(ans[0] + 0.5) == 1);
-    assert(int(ans[1] + 0.5) == 2);
-    assert(int(ans[2] + 0.5) == 3);
-
+    {
+        auto ans = SLAE::gauss(Matrix<double>(vector<vector<double>>{ { 4, 2, -1 }, { 2, 4, 3 }, { -1, 3, 5 } }), vector<double>{ 5, 19, 20 });
+        cout << ans << endl;
+        assert(int(ans[0] + 0.5) == 1);
+        assert(int(ans[1] + 0.5) == 2);
+        assert(int(ans[2] + 0.5) == 3);
+    }
+    {
+        vector<double> ans;
+        int cnt = SLAE::gaussGeneral(vector<vector<double>>{ { 4, 2, -1 }, { 2, 4, 3 }, { -1, 3, 5 } }, vector<double>{ 5, 19, 20 }, ans);
+        cout << "cnt = " << cnt << ", ans = " << ans << endl;
+        assert(cnt == 1);
+        assert(int(ans[0] + 0.5) == 1);
+        assert(int(ans[1] + 0.5) == 2);
+        assert(int(ans[2] + 0.5) == 3);
+    }
+    {
+        vector<double> ans;
+        int cnt = SLAE::gaussGeneral(vector<vector<double>>{ { 4, 2, 0, -1 }, { 2, 4, 0, 3 }, { -1, 3, 0, 5 } }, vector<double>{ 5, 19, 20 }, ans);
+        cout << "cnt = " << cnt << ", ans = " << ans << endl;
+        assert(cnt == SLAE::INF);
+        assert(int(ans[0] + 0.5) == 1);
+        assert(int(ans[1] + 0.5) == 2);
+        assert(int(ans[3] + 0.5) == 3);
+    }
+    {
+        vector<double> ans;
+        int cnt = SLAE::gaussGeneral(vector<vector<double>>{ { 4, 2, -1 }, { 2, 4, 3 }, { -1, 3, 5 }, { 1, 2, 3 } }, vector<double>{ 5, 19, 20, 40 }, ans);
+        cout << "cnt = " << cnt << ", ans = " << ans << endl;
+        assert(cnt == 0);
+    }
+    {
+        vector<double> ans;
+        int cnt = SLAE::gaussGeneral(vector<vector<double>>{ { 4, 2, -1 }, { 2, 4, 3 }, { 2, 4, 3 } }, vector<double>{ 5, 19, 20 }, ans);
+        cout << "cnt = " << cnt << ", ans = " << ans << endl;
+        assert(cnt == 0);
+    }
+    {
+        vector<double> ans;
+        int cnt = SLAE::gaussGeneral(vector<vector<double>>{ { 4, 2, -1 }, { 2, 4, 3 } }, vector<double>{ 5, 19 }, ans);
+        cout << "cnt = " << cnt << ", ans = " << ans << endl;
+        assert(cnt == SLAE::INF);
+    }
     cout << "OK!" << endl;
 }
--- library/math/gaussianElimination.h
+++ library/math/gaussianElimination.h
@@ -2,35 +2,108 @@
 
 #include "matrix.h"
 
-// O(N^3)
-vector<double> gauss(Matrix<double> a, vector<double> b) {
-    int N = a.N;
-    for (int row = 0; row < N; row++) {
-        int best = row;
-        for (int i = row + 1; i < N; i++) {
-            if (abs(a[best][row]) < abs(a[i][row]))
-                best = i;
-        }
+// SLAE = System of n linear algebraic equations
+struct SLAE {
+    // O(N^3), 'a' is a N x N matrix
+    static vector<double> gauss(Matrix<double> a, vector<double> b) {
+        const double EPS = 1e-9;
 
-        if (row != best) {
-            swap(a[row], a[best]);
-            swap(b[row], b[best]);
-        }
-
-        for (int i = row + 1; i < N; i++)
-            a[row][i] /= a[row][row];
-
-        b[row] /= a[row][row];
-        for (int i = 0; i < N; i++) {
-            double x = a[i][row];
-            if (i != row && x != 0) {
-                for (int j = row + 1; j < N; j++)
-                    a[i][j] -= a[row][j] * x;
-
-                b[i] -= b[row] * x;
+        int N = a.N;
+        for (int row = 0, col = 0; col < N; col++) {
+            int best = row;
+            for (int i = row + 1; i < N; i++) {
+                if (abs(a[best][col]) < abs(a[i][col]))
+                    best = i;
             }
+            if (abs(a[best][col]) < EPS)
+                continue;
+
+            if (row != best) {
+                swap(a[row], a[best]);
+                swap(b[row], b[best]);
+            }
+
+            auto x = a[row][col];
+            for (int i = col; i < N; i++)
+                a[row][i] /= x;
+            b[row] /= x;
+
+            for (int i = 0; i < N; i++) {
+                double x = a[i][col];
+                if (i != row && x != 0) {
+                    for (int j = col; j < N; j++)
+                        a[i][j] -= a[row][j] * x;
+                    b[i] -= b[row] * x;
+                }
+            }
+
+            row++;
         }
+
+        return b;
     }
 
-    return b;
-}
+    static const int INF = 0x3f3f3f3f;
+
+    // O(N^3), 'a' is a N x M matrix.
+    static int gaussGeneral(vector<vector<double>> a, vector<double> b, vector<double>& ans) {
+        const double EPS = 1e-9;
+
+        int N = (int)a.size();
+        int M = (int)a[0].size();
+
+        vector<int> where(M, -1);
+        for (int col = 0, row = 0; col < M && row < N; col++) {
+            int best = row;
+            for (int i = row; i < N; ++i) {
+                if (abs(a[best][col]) < abs(a[i][col]))
+                    best = i;
+            }
+            if (abs(a[best][col]) < EPS)
+                continue;
+
+            if (row != best) {
+                swap(a[row], a[best]);
+                swap(b[row], b[best]);
+            }
+
+            where[col] = row;
+
+            auto x = a[row][col];
+            for (int i = col; i < M; i++)
+                a[row][i] /= x;
+            b[row] /= x;
+
+            for (int i = 0; i < N; ++i) {
+                double x = a[i][col];
+                if (i != row && x != 0) {
+                    for (int j = col; j < M; ++j)
+                        a[i][j] -= a[row][j] * x;
+                    b[i] -= b[row] * x;
+                }
+            }
+
+            row++;
+        }
+
+        ans.assign(M, 0);
+        for (int i = 0; i < M; i++) {
+            if (where[i] >= 0)
+                ans[i] = b[where[i]];
+        }
+
+        for (int i = 0; i < N; i++) {
+            double sum = 0;
+            for (int j = 0; j < M; j++)
+                sum += ans[j] * a[i][j];
+            if (abs(sum - b[i]) > EPS)
+                return 0;
+        }
+
+        for (int i = 0; i < M; i++)
+            if (where[i] == -1)
+                return INF;
+
+        return 1;
+    }
+};
