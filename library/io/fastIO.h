bda5e05a0d30e88de88923bc17b5f80a3b43cabb
youngman <youngman.ro@gmail.com>
2019-07-30 02:33:09 +0900

    [Change] fast IO operations

--- library/io/fastIO.h
+++ library/io/fastIO.h
@@ -3,17 +3,20 @@
 #include <stdio.h>
 
 namespace FastIO {
-    char gInBuff[1 << 24];
+    const int SIZE = (1 << 24);
+
+    char gInBuff[SIZE];
     const char* gInPtr;
 
     void init() {
         gInPtr = gInBuff;
-        auto size = fread(gInBuff, 1, sizeof(gInBuff) - 1, stdin);
-        gInBuff[size] = 0;
+        auto sz = fread(gInBuff, 1, SIZE - 1, stdin);
+        gInBuff[sz] = 0;
     }
 
-    int readInt() {
-        unsigned u = 0, s = 0;
+    // with sign
+    int readSInt() {
+        unsigned int u = 0, s = 0;
 
         while (*gInPtr && *gInPtr <= 32)
             ++gInPtr;
@@ -26,10 +29,70 @@ namespace FastIO {
         }
 
         while (*gInPtr >= '0' && *gInPtr <= '9')
-            u = (u << 3) + (u << 1) + (*gInPtr++ - '0');
+            u = u * 10 + (*gInPtr++ - '0');
 
         return static_cast<int>((u ^ s) + !!s);
     }
+
+    int readInt() {
+        int res = 0;
+
+        while (*gInPtr && *gInPtr <= 32)
+            ++gInPtr;
+
+        while (*gInPtr >= '0' && *gInPtr <= '9')
+            res = res * 10 + (*gInPtr++ - '0');
+
+        return res;
+    }
+
+    // with sign
+    long long readSLL() {
+        unsigned long long u = 0, s = 0;
+
+        while (*gInPtr && *gInPtr <= 32)
+            ++gInPtr;
+
+        if (*gInPtr == '-') {
+            s = ~s;
+            ++gInPtr;
+        } else if (*gInPtr == '+') {
+            ++gInPtr;
+        }
+
+        while (*gInPtr >= '0' && *gInPtr <= '9')
+            u = u * 10 + (*gInPtr++ - '0');
+
+        return static_cast<long long>((u ^ s) + !!s);
+    }
+
+    long long readLL() {
+        long long res = 0;
+
+        while (*gInPtr && *gInPtr <= 32)
+            ++gInPtr;
+
+        while (*gInPtr >= '0' && *gInPtr <= '9')
+            res = res * 10 + (*gInPtr++ - '0');
+
+        return res;
+    }
+
+    struct Stream {
+        Stream() {
+            init();
+        }
+
+        Stream& operator >> (int& x) {
+            x = readInt();
+            return *this;
+        }
+
+        Stream& operator >> (long long& x) {
+            x = readLL();
+            return *this;
+        }
+    };
 }
 
 /*
@@ -42,4 +105,15 @@ int main() {
     int b = FastIO::readInt();
     // ...
 }
+
+OR
+
+int main() {
+    FastIO::Stream is;
+
+    int a, b;
+    is >> a >> b;
+    // ...
+}
+
 */
