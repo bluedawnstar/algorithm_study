5966e52342ad11bfb3377c484364be934829cedf
youngman <youngman.ro@gmail.com>
2019-09-20 01:47:16 +0900

    minor changes

--- library/polynomial/ntt.cpp
+++ library/polynomial/ntt.cpp
@@ -165,7 +165,7 @@ private:
 
 
 void testNTT() {
-    //return; //TODO: if you want to test, make this line a comment.
+    return; //TODO: if you want to test, make this line a comment.
 
     cout << "--- NTT ------------------------" << endl;
     {
