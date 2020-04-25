bff827a2b571d45a516f7743b0517d5c4db05775
youngman <youngman.ro@gmail.com>
2020-04-25 23:11:56 +0900

    minor changes

--- library/graph/basicUndirectedGraph.h
+++ library/graph/basicUndirectedGraph.h
@@ -180,6 +180,7 @@ struct BasicUndirectedGraph {
     //---
 
     // Lexicographic Breadth First Search (Lex-BFS), O(V + E)
+    // https://en.wikipedia.org/wiki/Lexicographic_breadth-first_search
     vector<int> findLexicographicBFS() {
         // initialize a sequence ¥Ò of sets, to contain a single set containing all vertices.
         struct ListT {
