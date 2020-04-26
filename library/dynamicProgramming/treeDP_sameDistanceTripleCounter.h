ce876d0ce32a1cbce20852093ce6eaedc6c05a3b
youngman <youngman.ro@gmail.com>
2020-04-26 17:44:16 +0900

    [Change] add explanation on SameDistanceTripleCounterOnTree

--- library/dynamicProgramming/treeDP_sameDistanceTripleCounter.h
+++ library/dynamicProgramming/treeDP_sameDistanceTripleCounter.h
@@ -6,6 +6,58 @@
 
     Find the number of triples of distinct nodes such that all three nodes are marked and
       all three pairs of nodes within the triple have the same distance.
+
+    1. property
+
+                u
+              /   \
+            ...   ...
+            /       \
+          leg        v
+                    /  \
+                  ...  ...
+                  /      \
+                fork    fork
+
+        a = u-leg, b = u-c, d = c-fork
+
+            d = a + b
+        <=> a = d - b
+
+    2. solution
+        dp1[u][i] = the number of (i == a)
+        dp2[u][i] = the number of (i == d - b)
+
+
+                u
+              /   \
+            ...    v
+            /     /  \ 
+          leg   ...  ...    
+                /      \
+              fork    fork
+
+        For each direct child v of node u,
+         
+        1) update v's dp tables
+          - increase depth of dp1 by 1
+          - decrease depth of dp2 by 1, because 'b' is increased by 1
+
+            dp1[v][i + 1] = dp1[v][i]
+            dp2[v][i - 1] = dp2[v][i]
+
+        2) update answer
+
+            ans += SUM (dp1[u][i] * dp2[v][i] + dp2[u][i] * dp1[v][i])
+                   i=1
+
+        3) merge v's dp tables to u's
+          - make new forks
+            dp2[u][i] += dp1[u][i] * dp1[v][i]
+
+          - directly merge v's tables to u's
+            dp2[u][i] += dp2[v][i]
+            dp1[u][i] += dp1[v][i]
 */
 struct SameDistanceTripleCounterOnTree {
     int N;
@@ -44,8 +96,8 @@ struct SameDistanceTripleCounterOnTree {
     }
 
 private:
-    vector<vector<long long>> dp1;  // leg
-    vector<vector<long long>> dp2;  // fork
+    vector<vector<long long>> dp1;  // leg, reversed
+    vector<vector<long long>> dp2;  // fork, reversed
 
     long long dfsSolve(int u, int parent) {
         long long res = 0;
@@ -58,9 +110,9 @@ private:
 
             res += dfsSolve(v, u);
 
-            // increase the depths by 1
-            dp1[v].push_back(0);
-            dp2[v].pop_back();
+            dp1[v].push_back(0);    // increase 'a' by 1
+            dp2[v].pop_back();      // decrease 'd - b' by 1, because 'b' is increased by 1
+
             // if v is deeper, switch the dp to make it faster
             if (dp1[v].size() > dp1[u].size()) {
                 swap(dp1[u], dp1[v]);
@@ -72,7 +124,8 @@ private:
                 dp2[u].insert(dp2[u].begin(), zeroes.begin(), zeroes.end());
             }
 
-            //--- update ans
+            //--- update answer
+
             // leg from v, fork from u
             for (int i = 1; i <= int(dp1[v].size()); i++)
                 res += dp1[v][dp1[v].size() - i] * dp2[u][dp2[u].size() - i];
@@ -80,11 +133,12 @@ private:
             for (int i = 1; i <= int(dp2[v].size()); i++)
                 res += dp2[v][dp2[v].size() - i] * dp1[u][dp1[u].size() - i];
 
-            //--- combine the dp
-            // new forks by combining 2 legs
+            //--- combine dp tables
+
+            // new forks
             for (int i = 1; i <= int(dp1[v].size()); i++)
                 dp2[u][dp2[u].size() - i] += dp1[u][dp1[u].size() - i] * dp1[v][dp1[v].size() - i];
-            // just add the rest together
+            // add v's tables to u's
             for (int i = 1; i <= int(dp1[v].size()); i++)
                 dp1[u][dp1[u].size() - i] += dp1[v][dp1[v].size() - i];
             for (int i = 1; i <= int(dp2[v].size()); i++)
