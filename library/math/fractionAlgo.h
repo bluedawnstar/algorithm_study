2d7e37b3b3cd2a733d0d595376d40d239a85905d
youngman <youngman.ro@gmail.com>
2020-08-18 01:22:00 +0900

    [Change] refactoring of lattice algorithms

--- library/math/fractionAlgo.h
+++ library/math/fractionAlgo.h
@@ -1,33 +1,5 @@
 #pragma once
 
-//--- Fraction Rank
-
-//CAUTION: use right type according to required precision (ex: long long, __int128_t)
-// sum(floor(a/b * i)), i=0..n
-inline long long sumFloor(long long n, long long a, long long b) {
-    //assert(gcd(a, b) == 1);
-
-    long long res = 0;
-    if (a >= b) {
-        res += (a / b) * n * (n + 1) / 2;
-        a %= b;
-    }
-    if (a == 0 || n == 0) {
-        return res;
-    }
-
-    long long m = a * n / b;
-    res += n * m + (n / b) - sumFloor(m, b, a);
-
-    return res;
-}
-
-// sum(floor(a/b * i)), i=left..right
-inline long long sumFloorRange(long long left, long long right, long long num, long long denom) {
-    return sumFloor(right, num, denom) - (left > 1 ? sumFloor(left - 1, num, denom) : 0);
-}
-
-
 //--- Continued Fraction
 
 template <typename T, typename R = T>
---
+++ library/math/latticePoints.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "fraction.h"
+
+struct LatticePoints {
+    /*
+                             n
+        sumFloor(a, b, n) = SUM floor(a / b * x) ,   x = 0, 1, 2, ..., n
+                            x=0
+    */
+    static long long count(long long a, long long b, long long n) {
+        //assert(gcd(a, b) == 1);
+        long long res = 0;
+        if (a >= b) {
+            res += (a / b) * n * (n + 1) / 2;
+            a %= b;
+        }
+        if (a == 0 || n == 0) {
+            return res;
+        }
+
+        long long m = a * n / b;
+        res += n * m + (n / b) - count(b, a, m);
+
+        return res;
+    }
+
+    static long long countRange(long long a, long long b, long long left, long long right) {
+        return count(a, b, right) - (left > 1 ? count(a, b, left - 1) : 0ll);
+    }
+
+    //---
+
+    /*
+                                n
+        sumFloor(a, b, c, n) = SUM floor(a / b * x + c) ,   x = 0, 1, 2, ..., n
+                               x=0
+    */
+    static long long count(long long a, long long b, long long c, long long n) {
+        //assert(gcd(a, b) == 1);
+        return (n + 1) * c + count(a, b, n);
+    }
+
+    static long long countRange(long long a, long long b, long long c, long long left, long long right) {
+        return count(a, b, c, right) - (left > 0 ? count(a, b, c, left - 1) : 0ll);
+    }
+
+    //---
+
+    /*
+                             n
+        sumFloor(a, b, n) = SUM floor((a.num / a.denom) * x + (b.num / b.denom)) ,   x = 0, 1, 2, ..., n
+                            x=0
+    */
+    static long long count(Fraction<long long> a, Fraction<long long> b, long long n) {
+        long long res = 0;
+
+        auto fa = a.floor();
+        auto fb = b.floor();
+        if (a.num >= a.denom || b.num >= b.denom) {
+            res += (fa * n + 2 * fb) * (n + 1) / 2;
+            a -= fa;
+            b -= fb;
+        }
+
+        auto t = a * (n + 1) + b;
+        t.reduce();
+
+        auto ft = t.floor();
+        if (ft >= 1) {
+            res += count(a.inverse(), (t - ft) * a.inverse(), ft - 1);
+        }
+
+        return res;
+    }
+
+    static long long countRange(Fraction<long long> a, Fraction<long long> b, long long left, long long right) {
+        return count(a, b, right) - (left > 0 ? count(a, b, left - 1) : 0ll);
+    }
+};
