92e107ac7083b20c864543585c56e4dc71f25e56
youngman <youngman.ro@gmail.com>
2020-11-17 01:58:22 +0900

    [Add] weighted bipartite matching

---
+++ library/graph/bipartiteWeightedMatching.cpp
@@ -0,0 +1,32 @@
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#include "bipartiteWeightedMatching.h"
+
+// <Related problems>
+// https://www.codechef.com/problems/RB2CNF
+
+/////////// For Testing ///////////////////////////////////////////////////////
+
+#include <cassert>
+#include <string>
+#include <unordered_set>
+#include <iostream>
+#include "../common/iostreamhelper.h"
+#include "../common/profile.h"
+#include "../common/rand.h"
+
+void testBipartiteWeightedMaching() {
+    return; //TODO: if you want to test, make this line a comment.
+
+    cout << "--- Bipartite Weighted Maching -------------" << endl;
+    {
+        //...
+    }
+
+    cout << "OK" << endl;
+}
