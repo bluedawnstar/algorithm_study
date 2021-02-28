22e25291449f8100afa37b32fd6bcd89e4441804
youngman <youngman.ro@gmail.com>
2021-03-01 02:08:54 +0900

    [Change] refactoring - move source file

--- library/combinatorics/main.cpp
+++ library/combinatorics/main.cpp
@@ -18,5 +18,6 @@ int main(void) {
     TEST(LatinSquare);
     TEST(MaxMatchingInMatrix);
     TEST(IntegerPartition);
+    //--- problems
     TEST(MinManhattanDistanceSet);
 }
--- library/combinatorics/problems/minManhattanDistanceSet.cpp
+++ library/combinatorics/problems/minManhattanDistanceSet.cpp
@@ -11,9 +11,9 @@ using namespace std;
 #include <cassert>
 #include <string>
 #include <iostream>
-#include "../common/iostreamhelper.h"
-#include "../common/profile.h"
-#include "../common/rand.h"
+#include "../../common/iostreamhelper.h"
+#include "../../common/profile.h"
+#include "../../common/rand.h"
 
 void testMinManhattanDistanceSet() {
     //return; //TODO: if you want to test, make this line a comment.
